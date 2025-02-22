f17b5e2ace7c42b9ba081832b54559efabd1b1f8
Wang Chao <wangchao2423@gmail.com>
2025-02-22 11:46:08 +0800

    样式功能

--- src/views/Form.vue
+++ src/views/Form.vue
@@ -3,7 +3,7 @@
  * @Author     : itchaox
  * @Date       : 2023-09-26 15:10
  * @LastAuthor : Wang Chao
- * @LastTime   : 2025-02-22 08:35
+ * @LastTime   : 2025-02-22 11:40
  * @desc       : Markdown 预览插件
 -->
 <script setup>
@@ -829,8 +829,10 @@
   }
 
   .answer-content {
-    background-color: #f0f7ff;
+    /* background-color: #f0f7ff; */
+    background-color: #fff;
     max-height: 50vh;
+    border: 1px solid #e5e6eb;
   }
 
   .question-content p {
